19f733761eb893ef3d8e661b3b9231295145d082
Jin19121 <jinkim19121@gmail.com>
2024-10-02 12:25:33 +0900

    전송방식

--- src/main/java/com/example/spring240924/controller/Controller20.java
+++ src/main/java/com/example/spring240924/controller/Controller20.java
@@ -1,7 +1,10 @@
 package com.example.spring240924.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -30,4 +33,26 @@ public class Controller20 {
         System.out.println("birth = " + birth);
         System.out.println("age = " + age);
     }
+
+    //    @RequestMapping("sub4", method= RequestMethod.GET)
+    @GetMapping("sub4")
+    public void get4() {
+        System.out.println("get 전송 방식");
+    }
+
+    //    @RequestMapping("sub4", method= RequestMethod.POST)
+    @PostMapping("sub4")
+    public void get4() {
+        System.out.println("post 전송 방식");
+    }
+
+    //연습
+    @GetMapping("sub5")
+    public void get5() {
+    }
+
+    @PostMapping("sub5")
+    public void get5() {
+    }
+
 }
