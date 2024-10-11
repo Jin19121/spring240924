260ed7ffb6097830e1bba305f06cc87c2ed96ea5
Jin19121 <jinkim19121@gmail.com>
2024-10-11 11:19:34 +0900

    DELETE

---
+++ sql/12delete.sql
@@ -0,0 +1,23 @@
+USE w3schools;
+
+#DELETE FROM 테이블명: 모든 레코드 삭제
+DELETE
+FROM Customers;
+
+SELECT *
+FROM Customers
+WHERE CustomerName = '이지훈';
+
+DELETE
+FROM Customers
+WHERE CustomerName = '이지훈';
+
+#꼭 DELETE 실행 전 같은 WHERE 절로 SELECT 해볼 것
+
+SELECT *
+FROM Customers
+ORDER BY CustomerID DESC;
+
+DELETE
+FROM Customers
+WHERE CustomerID = 104;
