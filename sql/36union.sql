cbdd25922cc303f5aa0d9d229a61e892e75eecd7
Jin19121 <jinkim19121@gmail.com>
2024-10-16 15:31:53 +0900

    union 합집합

---
+++ sql/36union.sql
@@ -0,0 +1,30 @@
+USE w3schools;
+
+#UNION : 합집합
+CREATE TABLE my_table54
+(
+    id   INT,
+    name VARCHAR(20)
+);
+CREATE TABLE my_table55
+(
+    id      INT,
+    address VARCHAR(20)
+);
+
+INSERT INTO my_table54
+VALUES (1, 'seoul'),
+       (2, 'hankang'),
+       (3, 'jeju');
+INSERT INTO my_table55
+VALUES (4, 'busan'),
+       (5, 'dokdo');
+
+SELECT *
+FROM my_table54
+UNION
+SELECT *
+FROM my_table55;
+
+INSERT INTO my_table55
+VALUES (3, 'jeju');
