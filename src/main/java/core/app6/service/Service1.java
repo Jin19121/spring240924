9ceac54da2a4f2f8c2d975c0c430e749675b6f39
Jin19121 <jinkim19121@gmail.com>
2024-10-17 14:12:26 +0900

    Spring bean 연습

---
+++ src/main/java/core/app6/service/Service1.java
@@ -0,0 +1,10 @@
+package core.app6.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Service1 {
+    public void yourTurn() {
+        System.out.println("Just Do It");
+    }
+}
