a745eed218a7ce88ffd2cbcf62af735382df6ff4
Jin19121 <jinkim19121@gmail.com>
2024-10-21 16:12:19 +0900

    dynamic sql: foreach 위치이동

--- src/main/java/com/example/spring240924/controller/Controller40.java
+++ src/main/java/com/example/spring240924/controller/Controller40.java
@@ -1,10 +1,12 @@
 package com.example.spring240924.controller;
 
-import com.example.spring240924.controller.dto.c40.Mapper10;
+import com.example.spring240924.mapper.Mapper10;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,10 +19,52 @@ public class Controller40 {
         System.out.println("두 개 값 모두");
         mapper10.select1("abc", "def");
 
-        System.out.println("name만");
+        System.out.println("name 만");
         mapper10.select1("abc", null);
 
         System.out.println("contact 만");
         mapper10.select1(null, "def");
     }
+
+    @GetMapping("sub2")
+    public void method2() {
+        System.out.println("모두");
+        mapper10.select2("abc", "def");
+        System.out.println("first");
+        mapper10.select2("abc", null);
+        System.out.println("second");
+        mapper10.select2(null, "def");
+    }
+
+    @GetMapping("sub3")
+    public void method3() {
+        mapper10.select3("abc", "def");
+        mapper10.select3("abc", null);
+        mapper10.select3(null, "def");
+        mapper10.select3(null, null);
+    }
+
+    @GetMapping("sub4")
+    public void method4() {
+        mapper10.select4("abc", "def");
+        mapper10.select4("abc", null);
+        mapper10.select4(null, "def");
+        mapper10.select4(null, null);
+    }
+
+    @GetMapping("sub5")
+    public void method5() {
+        mapper10.select5(List.of("germany", "usa", "uk"));
+        mapper10.select5(List.of("germany", "usa"));
+        mapper10.select5(List.of("germany"));
+    }
+
+    @GetMapping("sub6")
+    public void method6() {
+        mapper10.select6(List.of("seoul", "busan", "jeju"));
+        mapper10.select6(List.of("seoul", "busan"));
+        mapper10.select6(List.of("seoul"));
+    }
+
+
 }
---
+++ src/main/java/com/example/spring240924/mapper/Mapper10.java
@@ -0,0 +1,106 @@
+package com.example.spring240924.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface Mapper10 {
+
+    // dynamic sql
+    // if
+    @Select("""
+            <script>
+            SELECT *
+            FROM Customers
+            WHERE 
+                Country = 'USA'
+                <if test="name != null">
+                AND CustomerName = #{name}
+                </if>
+            
+                <if test="contact != null">
+                AND ContactName = #{contact}
+                </if>
+            </script>
+            """)
+    Map<String, Object> select1(String name, String contact);
+
+    @Select("""
+            <script>
+            SELECT *
+            FROM Employees
+            WHERE 
+                EmployeeID = 3
+                <if test="firstName !=null">
+                AND FirstName = #{firstName}
+                </if>
+                <if test="lastName !=null">
+                AND LastName = #{lastName}
+                </if>
+            </script>
+            """)
+    Map<String, Object> select2(String firstName, String lastName);
+
+    @Select("""
+            <script>
+                SELECT *
+                FROM Customers
+                <trim prefix="WHERE" prefixOverride="AND">
+                WHERE
+                    <if test="name != null">
+                        CustomerName = #{name}
+                    </if>
+                    <if test="contact != null">
+                        AND ContactName = #{contact}
+                    </if>
+                </trim>
+            </script>
+            """)
+    Map<String, Object> select3(String name, String contact);
+
+    @Select("""
+            <script>
+            SELECT *
+            FROM Employees
+            <trim prefix="WHERE" prefixOverride="OR">
+            WHERE
+                <if test="firstName !=null">
+                FirstName = #{firstName}
+                </if>
+                <if test="lastName !=null">
+                OR LastName = #{lastName}
+                </if>
+            </trim>
+            </script>
+            """)
+    Map<String, Object> select4(String firstName, String lastName);
+
+    @Select("""
+            <script>
+            SELECT * FROM Customers
+            <trim prefix="WHERE">
+            <foreach collection="countryList" item="country" separator=","
+            open="Country IN (" close=")">
+                #{country}
+            </foreach>
+            </trim>
+            </script>
+            """)
+    Object select5(List<String> countryList);
+
+    @Select("""
+            <script>
+                SELECT *
+                FROM Suppliers
+                <trim prefix="WHERE">
+                    <foreach collection="cityList" item="city" separator=", " open="City IN (" close=")">
+                        #{city}
+                    </foreach>
+                </trim>
+            </script>
+            """)
+    Object select6(List<String> cityList);
+}
