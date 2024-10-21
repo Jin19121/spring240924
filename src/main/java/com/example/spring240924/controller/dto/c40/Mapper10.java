23e070a8e19ffe805e2d6a6a4949ae7cdb3eb6e5
Jin19121 <jinkim19121@gmail.com>
2024-10-21 14:39:37 +0900

    dynamic sql: if

---
+++ src/main/java/com/example/spring240924/controller/dto/c40/Mapper10.java
@@ -0,0 +1,29 @@
+package com.example.spring240924.controller.dto.c40;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Map;
+
+@Mapper
+public interface Mapper10 {
+
+    //dynamic sql
+    //if
+    @Select("""
+            <script>            
+            SELECT * 
+            FROM Customers
+            WHERE 
+                Country = 'USA'
+                <if test="name !=null">
+                AND CustomerName = #{name}
+                </if>
+            
+                <if test="contact !=null">
+                AND ContactName = #{contact}
+                </if>
+                </script>
+            """)
+    Map<String, Object> select1(String name, String contact);
+}
