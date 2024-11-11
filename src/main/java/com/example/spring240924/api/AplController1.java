3e4c05752925515a9104420ec33b67d0ec97d654
Jin19121 <jinkim19121@gmail.com>
2024-11-11 16:27:16 +0900

    requestBody 요청 받기 예제 : 여러 데이터 타입

--- src/main/java/com/example/spring240924/api/AplController1.java
+++ src/main/java/com/example/spring240924/api/AplController1.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping("/api/main1")
@@ -31,4 +32,21 @@ public class AplController1 {
   public void sub4(@RequestBody Map<String, Object> map) {
     System.out.println(map);
   }
+
+  @PostMapping("sub6")
+  public void sub6(@RequestBody Map<String, Object> map) {
+    System.out.println(map);
+  }
+
+  @PostMapping("sub7")
+  public void sub7(@RequestBody List<Object> list) {
+    System.out.println(list);
+  }
+
+  @PostMapping("sub8")
+  public void sub8(@RequestBody List<Map<String, Object>> list) {
+    System.out.println(list);
+  }
+
+
 }
