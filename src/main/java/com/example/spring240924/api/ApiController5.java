1b2f330a9bf36587bd50a4331e2d02ade6e7842c
Jin19121 <jinkim19121@gmail.com>
2024-11-12 12:22:49 +0900

    response data(map=>object)

--- src/main/java/com/example/spring240924/api/ApiController5.java
+++ src/main/java/com/example/spring240924/api/ApiController5.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/api/main5")
 public class ApiController5 {
@@ -20,4 +22,17 @@ public class ApiController5 {
   public String sub3() {
     return "seoul";
   }
+
+  @GetMapping("sub4")
+  @ResponseBody
+  public Map<String, Object> sub4() {
+    return Map.of("name", "kim", "age", 28);
+  }
+
+  @GetMapping("sub5")
+  @ResponseBody
+  public Map<String, Object> sub5() {
+    return Map.of("title", "도겸 시점", "content", "street photos", "price", 45000, "available", false);
+  }
+
 }
