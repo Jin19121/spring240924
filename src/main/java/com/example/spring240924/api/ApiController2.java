690f8c39b989c84f8c6f7107c38b66b4e5e8320f
Jin19121 <jinkim19121@gmail.com>
2024-11-12 10:20:34 +0900

    javabean parsing (배열, 객체) 연습

--- src/main/java/com/example/spring240924/api/ApiController2.java
+++ src/main/java/com/example/spring240924/api/ApiController2.java
@@ -3,11 +3,13 @@ package com.example.spring240924.api;
 import com.example.spring240924.dto.a2.Book;
 import com.example.spring240924.dto.a2.Person;
 import com.example.spring240924.dto.a2.Player;
+import com.example.spring240924.dto.a2.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -26,5 +28,20 @@ public class ApiController2 {
   @PostMapping("sub3")
   public void method3(@RequestBody Player player) {
     System.out.println(player);
+  }
+
+  @PostMapping("sub4")
+  public void method4(@RequestBody Product product) {
+    System.out.println(product);
+  }
+
+  @PostMapping("sub5")
+  public void method5(@RequestBody List<String> items) {
+    System.out.println(items);
+  }
+
+  @PostMapping("sub6")
+  public void method6(@RequestBody List<Person> person) {
+    System.out.println(person);
   }
 }
