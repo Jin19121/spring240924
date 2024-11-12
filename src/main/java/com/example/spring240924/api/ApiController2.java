26d495a7237303231b67d701752f3d43725f53b5
Jin19121 <jinkim19121@gmail.com>
2024-11-12 09:38:57 +0900

    javabean parsing 연습

--- src/main/java/com/example/spring240924/api/ApiController2.java
+++ src/main/java/com/example/spring240924/api/ApiController2.java
@@ -1,5 +1,6 @@
 package com.example.spring240924.api;
 
+import com.example.spring240924.dto.a2.Book;
 import com.example.spring240924.dto.a2.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,5 +15,10 @@ public class ApiController2 {
   @PostMapping("sub1")
   public void method1(@RequestBody Person person) {
     System.out.println(person);
+  }
+
+  @PostMapping("sub2")
+  public void method2(@RequestBody Book book) {
+    System.out.println(book);
   }
 }
